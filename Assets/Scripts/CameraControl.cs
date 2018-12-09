71b8da7957b8a3af44c31fa9ba29706f58b2eb22
Michel Gutardo <michel_metal-sk8@hotmail.com>
2018-12-09 18:43:58 -0200

    Network updates

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class CameraControl : MonoBehaviour {
 
 	static public Transform player;
 	public bool locked;
--- Assets/Scripts/LocalPlayer.cs
+++ Assets/Scripts/LocalPlayer.cs
@@ -1,17 +1,73 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 
 public class LocalPlayer : NetworkBehaviour {
+
+    public Text playerName;
+    public Text nameLabel;
+    public Transform namePosition;
+    public string field_name = string.Empty;
+
+    [SyncVar (hook= "OnChangeName")]
+    public string sync_name = "player";
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Invoke("UpdateStates",1);
+    }
+
+    void UpdateStates()
+    {
+        OnChangeName(sync_name);
+    }
+
+    void OnChangeName(string name)
+    {
+        nameLabel.text = sync_name = name;
+    }
+
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        nameLabel.text = sync_name = newName;
+    }
+
+
+    void OnGUI()
+    {
+        if(isLocalPlayer){
+            field_name = GUI.TextField (new Rect(25,15,100,25),field_name);
+            if(GUI.Button(new Rect(130,15,35,25),"Set")){
+                CmdChangeName(field_name);
+            }
+        }
+    }
 
     void Start()
     {
         if(isLocalPlayer)
         {
             GetComponent<PlayerController>().enabled = true;
-            Camera.player =  this.gameObject.transform ;
+            CameraControl.player =  this.gameObject.transform ;
         }
+        else
+        {
+            GetComponent<PlayerController>().enabled = false;
+        }
+
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(playerName,Vector3.zero,Quaternion.identity) as Text;
+        nameLabel.transform.SetParent(canvas.transform);
     }
 
+    void Update()
+    {
+        Vector3 nameLabelPosition = Camera.main.WorldToScreenPoint(namePosition.position);
+        nameLabel.transform.position = nameLabelPosition;
+    }
 }
